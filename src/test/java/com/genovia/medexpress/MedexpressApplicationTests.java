7d3c2169c5b4b18525385618c84776384985dd56
Priyanka G <magarnanda@gmail.com>
2025-01-16 14:31:55 +0000

    Add files via upload

---
+++ src/test/java/com/genovia/medexpress/MedexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genovia.medexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
